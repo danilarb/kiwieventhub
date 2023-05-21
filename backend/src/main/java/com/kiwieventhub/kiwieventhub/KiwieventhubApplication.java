14a4941587d0ac87123ed0321b85742e456bfc96
Danila Bublik <dbublik11@gmail.com>
2023-05-21 13:45:25 +1200

    Initial commit

---
+++ backend/src/main/java/com/kiwieventhub/kiwieventhub/KiwieventhubApplication.java
@@ -0,0 +1,13 @@
+package com.kiwieventhub.kiwieventhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = { })
+public class KiwieventhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiwieventhubApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/kiwieventhub/kiwieventhub/KiwieventhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiwieventhub.kiwieventhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiwieventhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
